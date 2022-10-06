035de4236e3b3b0558837f3db9bb48c7581583f5
KcQuinco <60454485+KcQuinco@users.noreply.github.com>
2022-10-06 23:02:50 +0800

    Add files via upload

--- GUI 129.py
+++ GUI 129.py
@@ -11,18 +11,28 @@ class Window:
     def __init__(self, root):
         self.root = root
         self.root.title("Strings and DFA")
-        self.root.geometry("450x400")
+        #self.root.geometry("450x400")
 
-        self.frame = Frame(self.root)
+        #row0, "margins"
+        Label(self.root,text="     ").grid(row=0, column=0, columnspan=5)
+        Label(self.root,text="     ").grid(row=0, column=0, rowspan=5)
+        Label(self.root,text="     ").grid(row=0, column=5, rowspan=5)
+        Label(self.root,text="     ").grid(row=6, column=0, columnspan=5)
 
-        Label(self.root,text="Transition Table",anchor="center").grid(row=1, column=0)
-        Label(self.root,text="Input",anchor="center").grid(row=1, column=1)
-        Label(self.root,text="Output",anchor="center").grid(row=1, column=2)
+        #row1, buttons
+        button1 = Button(self.root, text="Load File", justify="center", padx="100", pady="10", command="load_file").grid(row=1, column=1, columnspan=2)
+        Label(self.root,text="     ").grid(row=1, column=3)
+        button2 = Button(self.root, text="Process", justify="center", padx="50", pady="10", command="process_file").grid(row=1, column=4)
 
-        button1 = Button(self.root, text="Load File", justify="center", padx="50", command="load_file")
-        button1.grid(row=0, column=1)
-        button2 = Button(self.root, text="Process", justify="center", padx="50", command="process_file")
-        button2.grid(row=0, column=2)
+        #row2, labels
+        Label(self.root,text="Transition Table", pady=10).grid(row=2, column=1, sticky="w")
+        Label(self.root,text="Input").grid(row=2, column=2, sticky="w")
+        Label(self.root,text="Output").grid(row=2, column=4, sticky="w")
+
+        #row3, trans table, input, output
+
+        #row4, status
+
 
     def load_file():
 
